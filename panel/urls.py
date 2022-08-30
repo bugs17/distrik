b14489823a2f79dbdd57b6a10dda42e1720d1846
bugs17 <rnaldi491@gmail.com>
2022-08-30 15:14:33 +0900

    urls.py delete user

--- panel/urls.py
+++ panel/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('user/',views.user_page, name='user'),
     path('updateuser/<str:pk>',views.update_user, name='updateuser'),
     path('changepassword/<str:pk>',views.changepassword, name='changepassword'),
+    path('deleteuser/<str:pk>',views.delete_user, name='deleteuser'),
     path('userprofile/',views.akun_seting, name='userprofile'),
     path('profileseting/',views.user_profile, name='profileseting'),
 
